8f9a9d5f8a3029609e27fe78a8772eef58005ba5
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-07-01 11:10:12 +0530

    Initialized chakra ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const colors = {
+  brand: {
+    50: "#ecefff",
+    100: "#cbceeb",
+    200: "#a9aed6",
+    300: "#888ec5",
+    400: "#666db3",
+    500: "#4d5499",
+    600: "#3c4178",
+    700: "#2a2f57",
+    800: "#181c37",
+    900: "#080819",
+  },
+};
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ colors, config });
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
